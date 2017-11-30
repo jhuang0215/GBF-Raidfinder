95fd7ce8cebded2f8dde7f7b30f1727ffcfb0acc
Jack Huang <jackhuangxl@gmail.com>
2017-11-29 16:33:23 -0800

    set up twit streaming

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+// steam a sample of public statuses
+var param = {
+    track: 'Lvl 75 Celeste Omega'
+}
+var stream = T.stream('statuses/filter', param);
+
+stream.on('tweet', function (tweet) {
+    console.log(tweet)
+})
