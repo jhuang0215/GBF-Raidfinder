dec7832aa2b1324cdfd08d5fbf64d3732667de37
Jack Huang <jackhuangxl@gmail.com>
2017-12-03 02:08:03 -0800

    finished grabbing and processing twitter data

--- index.js
+++ index.js
@@ -25,14 +25,82 @@ let param = {
 }
 let stream = T.stream('statuses/filter', param);
 
-stream.on('tweet', function (tweet) {
+stream.on('tweet', function(tweet){
     console.log(tweet);
+    if (isValidTweet(tweet)){
+        console.log('valid');
+        let raidInfo = getTweetMessage(tweet);
+        console.log(JSON.stringify(raidInfo));
+    }
 
 })
 
-/*
-function isValidTweet (data) {
-    let result = false;
-    if()
+
+function isValidTweet(data) {
+    if(data.source !== '<a href="http://granbluefantasy.jp/" rel="nofollow">グランブルー ファンタジー</a>'){
+        console.log('invalid tweet source');
+        return false;
+    } 
+    if(data.text === null){
+        console.log('tweet no text');
+        return false;
+    }
+
+
+    return true;
 }
-*/
+
+function getTweetMessage(tweet) {
+    let result = {
+        raidID: "",
+        user: "@" + tweet.user.screen_name,            
+        time: tweet.created_at,
+        raidType: searchTextForRaids(tweet.text),
+        message: "No Twitter Message",
+        language: "ja",
+        status: "unclicked"
+    };
+
+    /*
+    // Splitting the twitter 'text' string
+    // Example: 
+    // INPUT: 'tweeterMessage A1B8BBC8 :Battle ID\nI need backup!\nLvl 75 Celeste Omega\nhttps://t.co/cgqe3pUhC2'
+    // OUTPUT:  ['tweeterMessage A1B8BBC8 :Battle ID',
+    //           'I need backup!',
+    //           'Lvl 75 Celeste Omega',
+    //           'https://t.co/cgqe3pUhC2']
+    */
+    let splitStrnig = tweet.text.split("\n");
+
+    // getting raid ID & tweet message
+    try{
+        let temp = splitStrnig[0].split(":");
+
+        // string manipulation on the twitter data to get raidID
+        result.raidID = temp[0].substr(temp[0].length - 9, 8); 
+
+        if(temp[0].length - 9 > 0){     //have a twitter emssage
+            result.message = temp[0].substr(0, temp[0].length - 10);
+        }
+    }catch (error){
+        console.log(error);
+    }
+
+    result.language = tweet.lang;
+
+    return result;
+}
+
+function searchTextForRaids(text) {
+    let result = null;
+
+    for(let i = 0; i < raidConfig.length; i++){
+        if(text.indexOf(raidConfig[i].english) !== -1 || 
+        text.indexOf(raidConfig[i].japanese) !== -1){
+            result = raidConfig[i].room;
+            break;
+        }
+    }
+
+    return result;
+}
