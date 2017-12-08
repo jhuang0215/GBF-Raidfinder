b00d5d3f460fee80cbaf7f6dd0efdfc10b488958
Jack Huang <jackhuangxl@gmail.com>
2017-12-07 18:29:22 -0800

    set up reactDOM

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,14 @@
-alert('It works!!!');
+const React = require('react');
+const ReactDOM = require('react-DOM');
+
+// Creat component 
+class RaidFinderComponenet extends React.Component {
+    render() {
+        return (
+            <h1>Helloooooooo</h1>
+        );
+    }
+}
+
+// Put component into html page
+ReactDOM.render(<RaidFinderComponenet />, document.getElementById('raidfinder-wrapper'));
