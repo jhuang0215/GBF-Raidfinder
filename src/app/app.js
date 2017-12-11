04c6f155c5b93d20935e92c79507ef89c42d1def
Jack Huang <jackhuangxl@gmail.com>
2017-12-10 21:49:43 -0800

    added setting button component in react

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,62 @@
 const React = require('react');
 const ReactDOM = require('react-DOM');
 
+require('./css/app.css');
+
 // Creat component 
 class RaidFinderComponenet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openSetting: false,
+            raidLists: ['hello', 'world']
+        };
+    }
+
+    render() {
+        let raidLists = this.state.raidLists;
+        raidLists = raidLists.map(function(item, index){
+            return (
+                <li>{item}</li>
+            );
+        });// to be continued check out nest componenet
+
+        return (
+            <div className="container">            
+               {this.state.openSetting ? <RaidFilter /> : null}
+               <ul>{raidLists}</ul>               
+               <SettingButton onClick={this.bthClicked.bind(this)} />
+            </div>
+        );
+    }//render
+
+    // Custom functions
+    bthClicked() {
+        alert("clicked");
+        console.log("clicked");
+        this.setState({
+            openSetting: true
+        });
+    }
+}
+
+class RaidFilter extends React.Component {
     render() {
         return (
-            <h1>Helloooooooo</h1>
+            <div>
+
+            </div>
         );
+    }
+}
+
+class SettingButton extends React.Component {
+    render() {
+        return (
+            <button className="setting-btn" onClick={this.props.onClick} >
+            +
+            </button>            
+        );   
     }
 }
 
